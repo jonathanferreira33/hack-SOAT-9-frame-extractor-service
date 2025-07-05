24aeb53742df4c398d44fdebb4517660acee5d37
Jonathan Ferreira <jhouus33@gmail.com>
2025-07-05 07:57:42 -0300

    add aquivos iniciais

---
+++ src/main/java/com/hack_SOAT_9/frame_extractor_service/FrameExtractorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hack_SOAT_9.frame_extractor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameExtractorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameExtractorServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hack_SOAT_9/frame_extractor_service/service/VideoProcessorService.java
@@ -0,0 +1,2 @@
+package com.hack_SOAT_9.frame_extractor_service.service;public class VideoProcessorService {
+}
